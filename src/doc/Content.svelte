2bdad7ecb3f7a8080ee1a0b94bdea609b2f80821
Dominic Doty <doty.dominic@gmail.com>
2025-02-28 17:22:25 -0700

    WIP

--- src/doc/Content.svelte
+++ src/doc/Content.svelte
@@ -1,14 +1,12 @@
 <script lang="ts">
   import Content from "$doc/content.md";
 
-  console.log(Content);
+  // console.log(Content);
 
   // import { compile } from "mdsvex";
   // let Comp = await compile(Content);
   // console.log(Comp);
-
-  // TODO: Why is mdsvex not working????
 </script>
 
-<!-- <Content /> -->
+<Content />
 <!-- <Comp /> -->
